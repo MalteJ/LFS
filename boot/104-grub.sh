f4d9bcb93f3c0e43d087ebfcb599dd8187b5168e
Malte Janduda <malte.janduda@sap.com>
2022-08-08 15:09:43 +0200

    working VM

--- boot/104-grub.sh
+++ boot/104-grub.sh
@@ -8,6 +8,9 @@ set -x
 
 mkdir -p /boot/grub
 
+# set root=(hd0,gpt2)
+# search --no-floppy --fs-uuid --set 8b681c2f-a5fa-498d-8ffa-2aa5016d32fc
+
 cat > /boot/grub/grub.cfg << EOF
 # Begin /boot/grub/grub.cfg
 set default=0
@@ -15,7 +18,6 @@ set timeout=5
 
 insmod part_gpt
 insmod ext2
-set root=(hd0,gpt2)
 
 if loadfont /boot/grub/fonts/unicode.pf2; then
   set gfxmode=auto
@@ -24,7 +26,7 @@ if loadfont /boot/grub/fonts/unicode.pf2; then
 fi
 
 menuentry "GNU/Linux, Linux 5.15.59-lfs-11.1" {
-        linux   /boot/vmlinuz-5.15.59-lfs-11.1 root=UUID=8b681c2f-a5fa-498d-8ffa-2aa5016d32fc ro
+        linux   /boot/vmlinuz-5.15.59-lfs-11.1 root=/dev/vda2 ro console=tty0 console=ttyS0,115200 earlyprintk=ttyS0,115200
 }
 
 menuentry "Firmware Setup" {
